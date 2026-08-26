730a4c89b2a5033ee1e4adaeafbc480e70f71731
epoininee2013@gmail.com <epoininee2013@gmail.com>
2026-08-26 13:17:36 +0900

    Input 명령어 2

--- p0826/p0826_04.py
+++ p0826/p0826_04.py
@@ -1,14 +1,60 @@
+# 10진수를 2진수로 변경하는 명령어 (bin)
+print(bin(147)) 
+print(bin(5))
+
+# 2진수를 10진수로 출력하는 방법 
+print(int("101",2))
+
+print(100*100)
+
+a=9
+b=2
+print(a/b)
+print(a//b) # 몫 
+print(a%b) # 나머지 
+
+# a = 5
+# 짝수, 홀수인지? 
+print(5%2==0)
+print(5%2==1)
+
+a = 5
+print(a%2==0)
+print(a%2==1)
+
+a = int(input("숫자를 입력하세요."))
+print(a%2==0) 
+print(a%2==1) 
+
+
+
+
+#a,b = 1,2 
+#print(a,b)
+
+# a=b=1
+#print(a,b)
+
+# 에러
+# a=1, b=2
+#print(a,b)
+
 # 국어, 영어, 수학점수를 입력받아
 # 합계, 평균을 출력하시오
 # 합계:300, 평균:100
 
+name = input("이름을 입력하세요")
 kor = int(input("국어점수를 입력하세요."))
 eng = int(input("영어점수를 입력하세요."))
 math = int(input("수학점수를 입력하세요."))
+total = kor+eng+math
+avg = total/3
+name = "홍길동"
 
 print("합계:{}, 평균:{}".format(300, 100))
-
-
+print("합계:{}, 평균:{}".format(total, avg))
+print("합계:{}, 평균:{:2f}".format(total, avg, name))
+print("이름:{}, 합계:{}, 평균:{}".format(name, total, avg))
 
 
 
