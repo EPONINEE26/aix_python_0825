85da184e592a5a69e7e3f23551e424f512710e83
epoininee2013@gmail.com <epoininee2013@gmail.com>
2026-08-28 16:03:57 +0900

    반복문

---
+++ p0828/p0828_09.py
@@ -0,0 +1,37 @@
+# 반복문 (for, while)
+print(1) 
+print(1) 
+print(1) 
+print(1) 
+print(1) 
+
+# # for i(변수) in 범위:
+# for i in range(10): # 10번 반복 
+#     print(1)
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(5):
+#     print(i*10)    
+
+# for i in range(1,6):
+#       print(i) 
+
+# for i in range(0,10,2): # 시작점, 끝점, 간격
+#       print(i)
+
+# for i in [1,5,3,2]:
+#       print(i)
+
+# for i in "안녕하세요":
+#       print(i)
+
+# arr=list(range(1,11))
+# print(arr)
+
+for i in range(10):
+    print("안녕")
+
+for _ in range(10):
+    print("안녕") 
