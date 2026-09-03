6f5b920411c20050c2fc99bc487bbfd5b52a1b5d
epoininee2013@gmail.com <epoininee2013@gmail.com>
2026-09-03 09:39:28 +0900

    구구단

--- p0902/gugudan_02.py
+++ p0902/gugudan_02.py
@@ -1,11 +1,13 @@
 def gugudan_func():
-for step in range(2,10,3):
-    for i in range(step, min(step + 3,10)):
-        print(f"[{i}단]",end="\t")
-    print()
-    for i in range(1,10):
-        for j in range(step, min(step +3, 10)):
-            print("{}X{}={}".format(j,i,i*j),end="\t") # i,j 자리변경을로 출력이 바뀜
+    for step in range(2, 10, 3): 
+        for i in range(step, min(step + 3, 10)):
+            print(f"[{i}단]", end="\t")
+        print()
+        
+        for i in range(1, 10):
+            for j in range(step, min(step + 3, 10)):
+                print("{}X{}={}".format(j, i, i * j), end="\t")
+            print()
         print()
 
-    print()
+
