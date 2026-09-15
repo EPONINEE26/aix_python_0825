ceb905b127bdb0add0275efeaeaa67baccb564cb
epoininee2013@gmail.com <epoininee2013@gmail.com>
2026-09-15 09:21:35 +0900

    학생성적프로그램

--- p0911/main.py
+++ p0911/main.py
@@ -3,7 +3,7 @@ from stuFunc import *
 readStu()
 while True:
 
-    choice = main_screen() # main_screen 이름 에러가 10시간 넘도록 되어 여러 번 재설치 후 해결이 됨 
+    choice = main_screen() # main_screen 이름 에러가 몇 시간 넘도록 되어 여러 번 재설치 후 해결이 됨 
     if choice == 1:
         stu_input()
     elif choice ==2:
