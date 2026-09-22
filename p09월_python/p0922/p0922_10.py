c1c83916dbb7f53f182d90e74a42a978a7b2d9e9
epoininee2013@gmail.com <epoininee2013@gmail.com>
2026-09-22 16:38:26 +0900

    pip

--- p09월_python/p0922/p0922_10.py
+++ p09월_python/p0922/p0922_10.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+# python -m pip install selenium 설치 
+
 url = "https://www.melon.com/chart/index.htm"
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36'}
 res = requests.get(url,headers=headers)
